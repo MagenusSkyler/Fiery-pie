1aad1fad1411c2283d4c88de0bdc0de6b321a620
Magenus_Skyler <110354887+MagenusSkyler@users.noreply.github.com>
2022-09-03 17:05:39 +0600

    Updated on 9/3/2022
    Language name was changed to Pandabear

--- shell.py
+++ shell.py
@@ -1,14 +1,14 @@
-import Fiery_pie as pie
+import Pandabear as pbear
 
-print("Fiery-pie Interpreter [version - 1.0]", end="\n")
+print("Pandabear Interpreter [version - 1.0]", end="\n")
 print("Type [help] or [/?] to get help.", end="\n\n")
 
 while True:
-    text = input("Fiery-pie >>> ")
+    text = input("Pandabear >>> ")
     if text == "help" or text == "/?":
         print("Help is unavailable at this moment.")
         continue
-    result, error = pie.run('<stdin>', text)
+    result, error = pbear.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
