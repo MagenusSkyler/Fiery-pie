8e320c3b60033a7d66a9508998913997ba41f7aa
Magenus_Skyler <110354887+MagenusSkyler@users.noreply.github.com>
2022-09-03 18:19:32 +0600

    Updated on 9/3/2022
    Added commands in shell

--- shell.py
+++ shell.py
@@ -1,13 +1,38 @@
+# IMPORTS
+import os
 import Pandabear as pbear
 
+# ON START
+os.system("TITLE Pandabear")
 print("Pandabear Interpreter [version - 1.0]", end="\n")
 print("Type [help] or [/?] to get help.", end="\n\n")
 
+# MAIN LOOP
 while True:
-    text = input("Pandabear >>> ")
-    if text == "help" or text == "/?":
-        print("Help is unavailable at this moment.")
+    text = input("Pandabear >>> ")  # TAKE COMMAND INPUT
+    
+    # COMMAND INPUT CHECK FROM A - Z IN ASCENDING ORDER
+    if text == "clear":
+        if os.name == "nt":
+            os.system("cls")
+            continue
+        else:
+            os.system("clear")
+            continue
+    elif text == "exit":
+        break
+    elif text == "help" or text == "/?":
+        print("clear        Clears the screen.")
+        print("exit         Quits the Pandabear program (command interpreter).")
+        print("help, /?     Provides Help information.")
+        print("version      Shows Pandabear version.")
+        print(end="\n")     # ADD 1 LINE GAP AT THE END
         continue
+    elif text == "version":
+        print("Pandabear 1.0")
+        continue
+    
+    # CALL MAIN INTERPRETER FUNCTIONS
     result, error = pbear.run('<stdin>', text)
 
     if error: print(error.as_string())
