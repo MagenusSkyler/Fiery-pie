2fbb25972c07ba976223e4074b3eb4a3bfe3377b
Magenus_Skyler <110354887+MagenusSkyler@users.noreply.github.com>
2022-09-05 11:58:42 +0600

    Updated on 9/5/2022

--- shell.py
+++ shell.py
@@ -33,7 +33,13 @@ while True:
         continue
     
     # CALL MAIN INTERPRETER FUNCTIONS
+    if text.strip() == "": continue
     result, error = pbear.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
